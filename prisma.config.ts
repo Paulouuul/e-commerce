825b47db5ebad1ef3ca6b30e93aa8be366d6cfc2
Paulouuul <tebetpaulo91@yahoo.com>
2026-05-01 22:46:52 -0300

    edit prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,10 +6,12 @@ if (!DATABASE_URL) {
   throw new Error('DATABASE_URL não configurada no arquivo .env')
 }
 export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+    seed: 'tsx prisma/seed.ts',
+  },
   datasource: {
       url: DATABASE_URL,
-  },
-  migrations: {
-    seed: 'tsx prisma/seed.ts',
   },
 })
